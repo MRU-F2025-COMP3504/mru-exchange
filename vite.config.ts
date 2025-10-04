77268f944117d884269f9b190414b42a98a817a3
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-10-04 13:35:45 -0600

    feat: set up tech stack

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen to all addresses (i.e., connects containers with host)
+    port: 5173,
+    watch: {
+      usePolling: true, // Update on file changes
+    },
+  },
+});
