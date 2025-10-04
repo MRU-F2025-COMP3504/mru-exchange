718b7615bf5761e8fd2d66c2bc48c790ceb0d4e2
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-10-04 14:16:37 -0600

    feat: add react lints

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
@@ -12,14 +14,25 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      react.configs['recommended-typescript'],
+      reactDom.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
       eslintConfigPrettier,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+      ecmaVersion: 2025,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
   },
 ]);
