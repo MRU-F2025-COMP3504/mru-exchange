dfbcaecaedf37a2cc3d957e07d836f4eb501d118
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-10-13 12:52:05 -0600

    feat(products): decompose product to attributes and stock into shared/types

---
+++ src/shared/types/index.ts
@@ -0,0 +1,16 @@
+export interface ProductAttributes {
+  seller: number;
+  title: string;
+  description: string;
+  image: string;
+}
+
+export interface ProductStock {
+  price: number;
+  unit: number;
+}
+
+interface Product {
+  attributes: ProductAttributes;
+  stock: ProductStock;
+}
