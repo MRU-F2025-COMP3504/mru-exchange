cb369530a8878fb48a68ec25945251b30bdd88c9
ramos <113315631+cjaco906@users.noreply.github.com>
2025-10-29 17:11:55 -0600

    refactor(shared): move product, buyer, and seller to product module

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,21 +2,3 @@ export * from '@shared/types/database';
 export * from '@shared/types/property';
 export * from '@shared/types/result';
 export * from '@shared/types/table';
-
-export interface Product {
-  id: number;
-  seller: Seller;
-  title: string;
-  description: string;
-  image: string;
-  price: number;
-  stock: number;
-}
-
-export interface Buyer {
-  id: number;
-}
-
-export interface Seller {
-  id: number;
-}
