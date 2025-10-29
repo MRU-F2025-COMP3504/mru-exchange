450ad00737a3121597be55da452bcae9e558fa11
ramos <113315631+cjaco906@users.noreply.github.com>
2025-10-29 17:13:17 -0600

    refactor(shared): export filter and product modules

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,4 +1,6 @@
 export * from '@shared/types/database';
+export * from '@shared/types/filter';
+export * from '@shared/types/product';
 export * from '@shared/types/property';
 export * from '@shared/types/result';
 export * from '@shared/types/table';
