7df1df3a85b78b42069ba62a5a01570229966bb1
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-01 18:36:05 -0600

    refactor(shared): separate module and rename types

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,9 +1,1 @@
-import type { Result } from '@shared/types';
-
-export function ok<T, E>(data: T): Result<T, E> {
-  return { ok: true, data };
-}
-
-export function err<T, E>(error: E): Result<T, E> {
-  return { ok: false, error };
-}
+export * from '@shared/utils/result';
---
+++ src/shared/utils/result.ts
@@ -0,0 +1,11 @@
+import type {
+  Result,
+} from '@shared/types';
+
+export function ok<T, E>(data: T): Result<T, E> {
+  return { ok: true, data };
+}
+
+export function err<T, E>(error: E): Result<T, E> {
+  return { ok: false, error };
+}
