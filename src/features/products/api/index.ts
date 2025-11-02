4e4a23e71794a2ea4e486d3a237ab2ad3e824935
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-01 18:44:58 -0600

    feat(products): prepare api

--- src/features/products/api/index.ts
+++ src/features/products/api/index.ts
@@ -2,104 +2,88 @@ import { supabase } from '@shared/api';
 import type {
   CategoryAssignedProductTable,
   CategoryTagTable,
-  DatabaseQueryResult,
+  DatabaseQuery,
   PickOmit,
   ProductTable,
 } from '@shared/types';
-import { ok, err } from '@shared/utils';
+import { query } from '@shared/utils';
 import type { User } from '@supabase/supabase-js';
 
-/**
- * @param id the product identifier
- */
 export async function get(
   id: number,
   columns: string,
-): DatabaseQueryResult<ProductTable> {
-  const { data, error } = await supabase
+): DatabaseQuery<ProductTable> {
+  return query(await supabase
     .from('Product_Information')
     .select(columns as '*')
     .eq('id', id)
-    .single();
-
-  return error ? err(error) : ok(data);
+    .single());
 }
 
-/**
- * @param id the seller identifier
- */
 export async function getBySeller(
-  seller: User,
+  seller: PickOmit<User, 'id'>,
   columns: string,
-): DatabaseQueryResult<ProductTable[]> {
-  const { data, error } = await supabase
+): DatabaseQuery<ProductTable[]> {
+  return query(await supabase
     .from('Product_Information')
     .select(columns as '*')
-    .eq('user_id', seller.id);
-
-  return error ? err(error) : ok(data);
+    .eq('user_id', seller.id));
 }
 
 export async function add(
   product: Required<ProductTable>,
-): DatabaseQueryResult<null> {
-  const { error } = await supabase
+): DatabaseQuery<ProductTable> {
+  return query(await supabase
     .from('Product_Information')
-    .insert(product);
-
-  return error ? err(new Error('Failed to add a new product')) : ok(null);
+    .insert(product)
+    .select()
+    .single());
 }
 
-
-export async function remove(product: Required<ProductTable>): DatabaseQueryResult<null> {
-  const { error } = await supabase
+export async function remove(product: Required<ProductTable>): DatabaseQuery<ProductTable> {
+  return query(await supabase
     .from('Product_Information')
     .delete()
-    .eq('id', product.id);
-
-  return error ? err(error) : ok(null);
+    .eq('id', product.id)
+    .select()
+    .single());
 }
 
 export async function update(
   product: Partial<ProductTable>,
-): DatabaseQueryResult<ProductTable> {
-  const { data, error } = await supabase
+): DatabaseQuery<ProductTable> {
+  return query(await supabase
     .from('Product_Information')
     .update(product)
     .select()
-    .single();
-
-  return error ? err(error) : ok(data);
+    .single());
 }
 
-export async function categorize(product: PickOmit<ProductTable, 'id'>, ...categories: PickOmit<CategoryTagTable, 'id'>[]): DatabaseQueryResult<CategoryAssignedProductTable[]> {
+export async function categorize(product: PickOmit<ProductTable, 'id'>, ...categories: PickOmit<CategoryTagTable, 'id'>[]): DatabaseQuery<CategoryAssignedProductTable[]> {
   const id = product.id;
-  const existing = await supabase
+  const existing = query(await supabase
     .from('Category_Assigned_Products')
     .delete()
-    .eq('product_id', id);
+    .eq('product_id', id));
 
-  if (existing.error) {
-    return err(existing.error);
+  if (!existing.ok) {
+    return existing;
   }
 
-  const { data, error } = await supabase
+  return query(await supabase
     .from('Category_Assigned_Products')
     .insert(categories.map((category) => ({
       category_id: category.id,
       product_id: id,
     })))
-    .select();
-
-  return error ? err(error) : ok(data);
+    .select());
 }
 
-export async function getCategories(product: PickOmit<ProductTable, 'id'>): DatabaseQueryResult<CategoryAssignedProductTable[]> {
-  const { data, error } = await supabase
+export async function getCategories(product: PickOmit<ProductTable, 'id'>): DatabaseQuery<CategoryAssignedProductTable[]> {
+  return query(await supabase
     .from('Category_Assigned_Products')
     .select('*')
-    .eq('product_id', product.id);
-  return error ? err(error) : ok(data);
+    .eq('product_id', product.id));
 }
 
 /*
