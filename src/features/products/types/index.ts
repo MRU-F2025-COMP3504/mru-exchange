984c5511a2db07db5a8f72890477e2d8ec41b961
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-01 19:44:29 -0600

    feat(products): remove product filter types

--- src/features/products/types/index.ts
+++ src/features/products/types/index.ts
@@ -1,10 +1,3 @@
-export const ProductFilterType = {
-  CATEGORY: 'CATEGORY',
-  SELLER: 'SELLER',
-  PRICE_RANGE: 'PRICE_RANGE',
-  STOCK_RANGE: 'STOCK_RANGE',
-} as const;
-
 export interface ProductFilter {
   type: string;
   order: number;
