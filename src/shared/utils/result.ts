b5cfb82f0cafac3928c5d3cd7ec4339f6c88c00a
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-01 18:36:31 -0600

    feat(shared): add query function utils

--- src/shared/utils/result.ts
+++ src/shared/utils/result.ts
@@ -1,6 +1,8 @@
 import type {
+  DatabaseQueryResult,
   Result,
 } from '@shared/types';
+import type { PostgrestSingleResponse } from '@supabase/supabase-js';
 
 export function ok<T, E>(data: T): Result<T, E> {
   return { ok: true, data };
@@ -8,4 +10,16 @@ export function ok<T, E>(data: T): Result<T, E> {
 
 export function err<T, E>(error: E): Result<T, E> {
   return { ok: false, error };
+}
+
+export function query<T>(response: PostgrestSingleResponse<T>): DatabaseQueryResult<T> {
+  const { data, error } = response;
+
+  if (data) {
+    return ok(data);
+  } else if (error) {
+    return err(error);
+  }
+
+  return err(new Error('Undetermined query result'));
 }
