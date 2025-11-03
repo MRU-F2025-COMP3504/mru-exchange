e058c7fecebae022f41bde22546f7ab19d7d1c44
ramos <113315631+cjaco906@users.noreply.github.com>
2025-11-03 14:58:14 -0700

    feat(messaging): make api concise

--- src/features/messaging/api/chat.ts
+++ src/features/messaging/api/chat.ts
@@ -10,14 +10,22 @@ import {
   REALTIME_LISTEN_TYPES,
   type RealtimeChannel,
   type RealtimePostgresInsertPayload,
+  type User,
 } from '@supabase/supabase-js';
 import { query, view } from '@shared/utils';
 
-export async function get(id: number): DatabaseView<Chat> {
-  return view(await supabase.from('Chats').select('*').eq('id', id).single());
+export async function get(
+  chat: RequiredColumns<Chat, 'id'>,
+): DatabaseView<Chat> {
+  return view(
+    await supabase.from('Chats').select('*').eq('id', chat.id).single(),
+  );
 }
 
-export async function getByUser(id: string): DatabaseView<Chat[]> {
+export async function getByUser(
+  user: RequiredColumns<User, 'id'>,
+): DatabaseView<Chat[]> {
+  const id = user.id;
   return view(
     await supabase
       .from('Chats')
@@ -61,13 +69,16 @@ export function subscribe(
     .subscribe();
 }
 
-export async function create(a: string, b: string): DatabaseQuery<Chat, 'id'> {
+export async function create(
+  a: RequiredColumns<User, 'id'>,
+  b: RequiredColumns<User, 'id'>,
+): DatabaseQuery<Chat, 'id'> {
   return query(
     await supabase
       .from('Chats')
       .insert({
-        user_id_1: a,
-        user_id_2: b,
+        user_id_1: a.id,
+        user_id_2: b.id,
       })
       .select()
       .single(),
