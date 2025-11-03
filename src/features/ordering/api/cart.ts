b511b368f0fece58c0d31388b6b7539d749da3e6
ramos <113315631+cjaco906@users.noreply.github.com>
2025-11-03 14:32:15 -0700

    refactor: reflect name changes

--- src/features/ordering/api/cart.ts
+++ src/features/ordering/api/cart.ts
@@ -2,48 +2,57 @@ import type {
   DatabaseQuery,
   DatabaseQueryArray,
   DatabaseView,
-  PickOmit,
   Product,
-  ProductOrder,
+  RequiredColumns,
   ShoppingCart,
+  ShoppingCartProduct,
 } from '@shared/types';
 import type { User } from '@supabase/supabase-js';
 import { supabase } from '@shared/api';
-import { query, view } from '@shared/api/database.ts';
+import { query, view } from '@shared/utils';
 
-export async function get(user: PickOmit<User, 'id'>): DatabaseView<ShoppingCart> {
+export async function get(
+  user: RequiredColumns<User, 'id'>,
+): DatabaseView<ShoppingCart> {
   return view(
     await supabase
       .from('Shopping_Cart')
       .select('*')
       .eq('user_id', user.id)
-      .single()
+      .single(),
   );
 }
 
-export async function getProducts(cart: PickOmit<Product, 'id'>): DatabaseView<ProductOrder[]> {
+export async function getProducts(
+  cart: RequiredColumns<Product, 'id'>,
+): DatabaseView<ShoppingCartProduct[]> {
   return view(
     await supabase
       .from('Shopping_Cart_Products')
       .select('*')
       .eq('shopping_cart_id', cart.id)
-      .select()
-  )
+      .select(),
+  );
 }
 
-export async function register(user: PickOmit<User, 'id'>): DatabaseQuery<ShoppingCart, 'id'> {
+export async function register(
+  user: RequiredColumns<User, 'id'>,
+): DatabaseQuery<ShoppingCart, 'id'> {
   return query(
     await supabase
       .from('Shopping_Cart')
       .insert({
-        user_id: user.id
+        user_id: user.id,
       })
       .select('id')
-      .single()
-  )
+      .single(),
+  );
 }
 
-export async function store(cart: PickOmit<ShoppingCart, 'id'>, product: PickOmit<Product, 'id'>): DatabaseView<ProductOrder> {
+export async function store(
+  cart: RequiredColumns<ShoppingCart, 'id'>,
+  product: RequiredColumns<Product, 'id'>,
+): DatabaseView<ShoppingCartProduct> {
   return query(
     await supabase
       .from('Shopping_Cart_Products')
@@ -52,11 +61,14 @@ export async function store(cart: PickOmit<ShoppingCart, 'id'>, product: PickOmi
         product_id: product.id,
       })
       .select()
-      .single()
-  )
+      .single(),
+  );
 }
 
-export async function remove(cart: PickOmit<ShoppingCart, 'id'>, product: PickOmit<Product, 'id'>): DatabaseView<ProductOrder> {
+export async function remove(
+  cart: RequiredColumns<ShoppingCart, 'id'>,
+  product: RequiredColumns<Product, 'id'>,
+): DatabaseView<ShoppingCartProduct> {
   return query(
     await supabase
       .from('Shopping_Cart_Products')
@@ -64,16 +76,18 @@ export async function remove(cart: PickOmit<ShoppingCart, 'id'>, product: PickOm
       .eq('shopping_cart_id', cart.id)
       .eq('product_id', product.id)
       .select()
-      .single()
-  )
+      .single(),
+  );
 }
 
-export async function clear(cart: PickOmit<ShoppingCart, 'id'>): DatabaseQueryArray<ProductOrder, 'product_id'> {
+export async function clear(
+  cart: RequiredColumns<ShoppingCart, 'id'>,
+): DatabaseQueryArray<ShoppingCartProduct, 'product_id'> {
   return query(
     await supabase
       .from('Shopping_Cart_Products')
       .delete()
       .eq('shopping_cart_id', cart.id)
-      .select()
-  )
+      .select(),
+  );
 }
