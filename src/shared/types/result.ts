ca9f647ef3851da5642be3e4ae6b23c34fbbfc2e
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-02 18:38:49 -0700

    feat(shared): add more database result types

--- src/shared/types/result.ts
+++ src/shared/types/result.ts
@@ -1,4 +1,10 @@
+import type { PickOmit } from '@shared/types/property.ts';
+
 export type Result<T, E> = { ok: true; data: T } | { ok: false; error: E };
 export type PromiseResult<T, E> = Promise<Result<T, E>>;
-export type DatabaseQuery<T> = PromiseResult<T, Error>;
-export type DatabaseQueryResult<T> = Result<T, Error>;
+export type DatabaseQueryResult<T, P extends keyof T> = Result<PickOmit<T, P>, Error>;
+export type DatabaseQuery<T, P extends keyof T> = Promise<DatabaseQueryResult<T, P>>;
+export type DatabaseQueryArrayResult<T, P extends keyof T> = Result<PickOmit<T, P>[], Error>;
+export type DatabaseQueryArray<T, P extends keyof T> = Promise<DatabaseQueryArrayResult<T, P>>;
+export type DatabaseViewResult<T> = Result<T, Error>;
+export type DatabaseView<T> = PromiseResult<T, Error>;
