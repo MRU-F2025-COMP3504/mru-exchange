7bd7c166b8f931fd0d5044ba6f8a8c9f94745fcb
AndrewTries <akraw904@mtroyal.ca>
2025-11-04 10:03:59 -0700

    feat: added generate_false_user function to generate authenticated users in database

--- database/generate_auth_users.sql
+++ database/generate_auth_users.sql
@@ -14,3 +14,28 @@ BEGIN
 RETURN password;
 END;
 $$ LANGUAGE plpgsql;
+
+
+-- generates authenticated users
+CREATE OR REPLACE FUNCTION mru_test.generate_false_user(email text, password text)
+RETURNS uuid AS $$
+declare
+    user_id uuid;
+    encrypted_pw text;
+BEGIN
+    user_id := gen_random_uuid();
+    encrypted_pw := crypt(password, gen_salt('bf', 12));
+
+INSERT INTO auth.users
+    (instance_id, id, aud, role, email, encrypted_password, email_confirmed_at, recovery_sent_at, last_sign_in_at, raw_app_meta_data, raw_user_meta_data, created_at, updated_at, confirmation_token, email_change, email_change_token_new, recovery_token)
+  VALUES
+    ('00000000-0000-0000-0000-000000000000', user_id, 'authenticated', 'authenticated', email, encrypted_pw, NOW(), NOW(), NOW(), '{"provider":"email","providers":["email"]}', '{}', NOW(), NOW(), '', '', '', '');
+  
+INSERT INTO auth.identities (id, user_id, provider_id, identity_data, provider, last_sign_in_at, created_at, updated_at)
+  VALUES
+    (gen_random_uuid(), user_id, user_id,
+        jsonb_build_object('sub', user_id::text, 'email', email),
+        'email', NOW(), NOW(), NOW());
+RETURN user_id;
+END;
+$$ LANGUAGE plpgsql;
