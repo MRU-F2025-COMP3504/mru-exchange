e17d606511567e3f3161fd198bc821672f9ff498
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-04 18:32:40 -0700

    feat(result): add default error type

--- src/shared/types/result.ts
+++ src/shared/types/result.ts
@@ -1,2 +1,2 @@
-export type Result<T, E> = { ok: true; data: T } | { ok: false; error: E };
+export type Result<T, E = Error> = { ok: true; data: T } | { ok: false; error: E };
 export type PromiseResult<T, E> = Promise<Result<T, E>>;
