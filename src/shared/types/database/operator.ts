5f35614a7e77566ab81d6a8e0674220bf3b24631
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-04 20:42:31 -0700

    refactor: remove database error types

--- src/shared/types/database/operator.ts
+++ src/shared/types/database/operator.ts
@@ -4,9 +4,10 @@ export type RequiredColumns<T, K extends keyof T> = Pick<T, K> & Partial<Omit<T,
 export type DatabaseQueryResult<T, P> =
   T extends (infer R)[]
     ? P extends keyof R
-      ? Result<RequiredColumns<R, P>[], Error>
-      : Result<R[], Error>
+      ? Result<RequiredColumns<R, P>[]>
+      : Result<R[]>
     : P extends keyof T
-      ? Result<RequiredColumns<T, P>, Error>
-      : Result<T, Error>;
+      ? Result<RequiredColumns<T, P>>
+      : Result<T>;
 export type DatabaseQuery<T, P> = Promise<DatabaseQueryResult<T, P>>;
+export type DatabaseQueryResponse<T, P extends keyof T> = Result<T> | Result<RequiredColumns<T, P>>;
