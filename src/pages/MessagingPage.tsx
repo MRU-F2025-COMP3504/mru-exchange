a8376f39c95ce07df668910a27ca99712cc1c4b5
Mjohn572 <mjohn572@mtroyal.ca>
2025-11-08 15:21:49 -0700

    Created new default messaing page 	new file:   src/pages/MessagingPage.tsx

---
+++ src/pages/MessagingPage.tsx
@@ -0,0 +1,15 @@
+import { useState, type FormEvent } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import Header from './Header';
+
+export default function MessagingPage() {
+    
+
+
+    return (
+        <div style={{ minHeight: '100vh', backgroundColor: '#F9FAFB' }}>
+
+        <Header/>
+        </div>
+    )
+}   
