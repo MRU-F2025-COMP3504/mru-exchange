248dd9c5df9dad66d188aba7b8517a1128c37364
Mjohn572 <mjohn572@mtroyal.ca>
2025-11-08 15:22:07 -0700

    Updated Product page divs 	modified:   src/pages/ProductPage.tsx

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,6 +1,15 @@
 import { useState, type FormEvent } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import Header from './Header';
 
     export default function ProductPage() {
-        
-    }
+    
+
+
+    return (
+        <div style={{ minHeight: '100vh', backgroundColor: '#F9FAFB' }}>
+
+        <Header/>
+        </div>
+    )
+}   
