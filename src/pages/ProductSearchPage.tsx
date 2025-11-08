07619aa40a2d53e62e44a0cc19ce5ec64022b94c
Mjohn572 <mjohn572@mtroyal.ca>
2025-11-08 15:22:45 -0700

    Created default page 	modified:   src/pages/ProductSearchPage.tsx

--- src/pages/ProductSearchPage.tsx
+++ src/pages/ProductSearchPage.tsx
@@ -1,6 +1,22 @@
 import { useState, type FormEvent } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import Header from './Header';
 
-    export default function ProductSearchPage() {
+export default function ProductSearchPage() {
         
-    }
+    return (
+        <div style={{ minHeight: '100vh', backgroundColor: '#F9FAFB' }}>
+
+            <Header/>
+
+            <main
+            style={{
+            maxWidth: '1200px',
+            margin: '0 auto',
+            padding: '2rem',
+            }}
+            ></main>
+        
+        </div>
+    )
+}
