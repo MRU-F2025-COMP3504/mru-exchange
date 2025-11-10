4ccaf15ba534e000d0e2d45a768cc6d3cf8dc0f5
Kyle Chen <150177160+Existenial@users.noreply.github.com>
2025-11-10 12:14:11 -0700

    Removed useless imports

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,5 +1,5 @@
-import { useState, type FormEvent } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
 
