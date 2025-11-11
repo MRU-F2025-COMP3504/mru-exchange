bc43b863947a20d08684011a0ecd635889902eb2
AndrewTries <akraw904@mtroyal.ca>
2025-11-10 19:51:24 -0700

    Added temp layout to profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,9 +1,14 @@
 import { useState, type FormEvent } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import Header from './Header';
+import { supabase } from '@shared/api';
 
 export default function ProfilePage() {
     
-    return(
-        <div></div>
+    return (
+            <div style={{ minHeight: '100vh', backgroundColor: '#F9FAFB' }}>
+    
+            <Header/>
+            </div>
     )
 }
