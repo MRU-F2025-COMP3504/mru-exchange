9596a7a272710fbeddf2569dd0160bd5a0197d46
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-11 21:43:48 -0700

    fix(review): remove unused publish param

--- src/features/review/types/index.ts
+++ src/features/review/types/index.ts
@@ -3,5 +3,5 @@ import type { DatabaseQuery, Result, Review } from '@shared/types';
 export interface ReviewPublisher {
   description(description: string): Result<this>;
   rating(rating: number): Result<this>;
-  publish(rating: number): DatabaseQuery<Review, 'id'>;
+  publish(): DatabaseQuery<Review, 'id'>;
 }
