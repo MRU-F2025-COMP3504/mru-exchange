97ff9787eaa93e0b27c78ceaf2d5c34bfcab5da7
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-14 20:16:38 -0700

    refactor: rename table names to reflect current specification changes

--- src/shared/types/database/table.ts
+++ src/shared/types/database/table.ts
@@ -1,4 +1,4 @@
-import type { Tables } from '@shared/types/database/schema.ts';
+import type { Tables } from '@shared/types/database/schema';
 
 export type UserProfile = Tables<'User_Information'>;
 export type Product = Tables<'Product_Information'>;
@@ -6,8 +6,8 @@ export type Chat = Tables<'Chats'>;
 export type UserMessage = Tables<'Messages'>;
 export type Category = Tables<'Category_Tags'>;
 export type CategorizedProduct = Tables<'Category_Assigned_Products'>;
-export type ShoppingCart = Tables<'Shopping_Cart'>;
-export type ShoppingCartProduct = Tables<'Shopping_Cart_Products'>;
+export type ProductBookmarker = Tables<'Shopping_Cart'>;
+export type BookmarkedProduct = Tables<'Shopping_Cart_Products'>;
 export type Review = Tables<'Reviews'>;
 export type UserReport = Tables<'Reports'>;
-export type UserInteraction = Tables<'User_Interactions'>;
+export type InteractingUsers = Tables<'User_Interactions'>;
