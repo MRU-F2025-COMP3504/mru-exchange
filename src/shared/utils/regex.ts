41b8fdc523054669fd8bd3f1a6b2d8d152f97023
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-16 12:25:59 -0700

    feat(regex): add email address regex and improve doc

--- src/shared/utils/regex.ts
+++ src/shared/utils/regex.ts
@@ -1,21 +1,26 @@
-// Title: Regular Expression to Validate File Path and Extension (remodeled for unix path)
-// Submitter: anonymous
-// Link: https://regex101.com/r/ZaU81J/1
-// Valid states:
-//  - `c:/path/to/file`
-//  - `/path/to/file`
-//  - `path/to/file`
-//  - `./path/to/file`
-//  - `../path/to/file`
-//  - `.././path/to/file`
-//  - `../../path/to/file`
-
 /**
- * Matches the image file path with the following regular expression.
+ * Validates the given image file path.
+ * Use {@link RegExp.test()} to match the given image file path.
+ *
+ * @see {@link https://regex101.com/r/ZaU81J/1} (Submitter: Anonymous)
+ * @returns the {@link RegExp} object
  */
 export const REGEX_IMAGE_PATH = /^(\w|\.|\/[a-z_\-\s0-9.]+)+\.(png|jpg)$/i;
 
 /**
- * Matches the text file path with the following regular expression.
+ * Validates the given text file path with the following regular expression.
+ * Use {@link RegExp.test()} to match the given text file path.
+ *
+ * @see {@link https://regex101.com/r/ZaU81J/1} (Submitter: Anonymous)
+ * @returns the {@link RegExp} object
  */
 export const REGEX_TEXT_PATH = /^(\w|\.|\/[a-z_\-\s0-9.]+)+\.(txt)$/i;
+
+/**
+ * Validates the given email address.
+ * Use {@link RegExp.test()} to match the given email address.
+ *
+ * @see {@link https://regex101.com/r/mX1xW0/1} (Submitter: jago)
+ * @returns the {@link RegExp} object
+ */
+export const REGEX_EMAIL = /^([\w-]+(?:\.[\w-]+)*)@(mtroyal\.ca)$/i;
