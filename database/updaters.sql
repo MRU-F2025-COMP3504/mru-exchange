68ec8015daba9d1d9935d12742fa1575588212ca
AndrewTries <akraw904@mtroyal.ca>
2025-11-16 23:31:58 -0700

    added function to automatically generate user shopping cart when user is added

--- database/updaters.sql
+++ database/updaters.sql
@@ -19,3 +19,19 @@ $$;
 create or replace trigger on_rating_update
 after insert on mru_dev."Reviews" for each row
 execute function mru_dev.update_user_rating();
+
+CREATE OR REPLACE FUNCTION mru_dev.create_shopping_cart()
+RETURNS trigger
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = mru_dev, auth
+AS $$
+BEGIN
+  insert into mru_dev."Shopping_Cart"(user_id)
+  values(
+    new.id)
+    on conflict (user_id)
+    do update set user_id = new.id;
+    return new;
+END;
+$$;
