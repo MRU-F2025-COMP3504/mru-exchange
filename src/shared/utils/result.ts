c7d5dfdac9b1af47cfef566489bb7ffe88d74a55
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-25 21:37:50 -0700

    feat(result): removed present()
    Using err() makes more sense

--- src/shared/utils/result.ts
+++ src/shared/utils/result.ts
@@ -42,28 +42,3 @@ export function err<T, E extends Error>(error: E | string): Result<T, E> {
 export function empty<T, E extends Error>(): NullableResult<T, E> {
   return { data: null, error: null };
 }
-
-/**
- * A utility function that represents a "non-empty" {@link Result}.
- * Useful for evaluating the existence of the given payload disregarding the error message.
- *
- * The {@link present()} evaluates the given data with either the two output types:
- * - The `data` payload property contains the given data that is not `null` nor `undefined`.
- *    - If the given data is truthy, the payload property evaluates to the given payload.
- * - The `error` payload property contains the error message, indicating the given data as `null` or `undefined`.
- *    - If the given data is falsy, the payload property evaluates to the pre-defined error message.
- *
- * Compatible with all {@link Result} variants.
- *
- * @param data the given payload
- * @returns the wrapped result that may contain the given payload
- * @see {@link Result} and {@link PromiseResult} for more information
- * @see {@link present()} that performs the opposite evaluation
- */
-export function present<T>(data: T | null | undefined): Result<T> {
-  if (data) {
-    return ok(data);
-  }
-
-  return err('Specified data is not present');
-}
