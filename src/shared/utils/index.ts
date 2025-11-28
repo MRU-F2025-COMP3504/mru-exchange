a3c86a3ee9db045bfb1eb1d4a8355a15b24a0ddf
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-27 17:56:17 -0700

    feat(utils): add form utility functions

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,4 +1,5 @@
 export * from '@shared/utils/database';
+export * from '@shared/utils/form';
 export * from '@shared/utils/hooks';
 export * from '@shared/utils/regex';
 export * from '@shared/utils/result';
