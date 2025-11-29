228c2c58ca2efb0dfdf95ab8fb92872bbb279e43
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-11-29 16:37:42 -0700

    feat(form): add getStrings() and getFiles()

--- src/shared/utils/form.ts
+++ src/shared/utils/form.ts
@@ -2,11 +2,29 @@ import type { Result } from '@shared/types';
 import { err, ok } from '@shared/utils';
 
 interface FormUtils {
+  getStrings: (form: FormData, key: string) => Result<string[]>;
   getString: (form: FormData, key: string) => Result<string>;
   getFile: (form: FormData, key: string) => Result<File>;
+  getFiles: (form: FormData, key: string) => Result<File[]>;
 }
 
 export const FormUtils: FormUtils = {
+  getStrings(form: FormData, key: string): Result<string[]> {
+    const input = form.getAll(key);
+    const values: string[] = [];
+
+    for (const value of input) {
+      if (typeof value !== 'string') {
+        return err(
+          `Invalid string detected in the form data (key: ${key}, input: ${JSON.stringify(input)})`,
+        );
+      }
+
+      values.push(value.trim());
+    }
+
+    return ok(values);
+  },
   getString(form: FormData, key: string): Result<string> {
     const input = form.get(key);
 
@@ -15,8 +33,21 @@ export const FormUtils: FormUtils = {
     }
 
     return err(
-      `No string found in the form data (key: ${JSON.stringify(input)})`,
+      `No string found in the form data (key: ${key}, input: ${JSON.stringify(input)})`,
     );
+  },
+  getFiles(form: FormData, key: string): Result<File[]> {
+    const input = form.getAll(key);
+
+    for (const value of input) {
+      if (typeof value === 'string') {
+        return err(
+          `Invalid file detected in the form data (key: ${key}, input: ${JSON.stringify(input)})`,
+        );
+      }
+    }
+
+    return ok(input as File[]);
   },
   getFile(form: FormData, key: string): Result<File> {
     const input = form.get(key);
@@ -25,6 +56,6 @@ export const FormUtils: FormUtils = {
       return ok(input);
     }
 
-    return err(`No file found in the form data (key: ${input})`);
+    return err(`No file found in the form data (key: ${key}, input: ${input})`);
   },
 };
